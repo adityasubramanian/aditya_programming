de1af8967b7fcfa7d9fc701aca1a6aadf0d3ad95
Aditya <aditya.fly@gmail.com>
2015-07-10 18:30:05 -0700

    Compiling all files related to data structures.

---
+++ datastructures/insertnodeathead.cpp
@@ -0,0 +1,33 @@
+/*
+  Insert Node at the begining of a linked list
+  Initially head pointer argument could be NULL for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+return back the pointer to the head of the linked list in the below method.
+*/
+Node* Insert(Node *head,int data)
+{
+  Node* current = new Node; 
+  current -> data = data; 
+
+  if (current -> next != NULL) {
+  	current -> next = NULL; 
+  }
+
+  if (head == NULL) {
+  	head == current; 
+  } else {
+  	if (head != NULL) {
+  		current -> next = head; 
+  		head = current; 
+  	} 
+  }
+  return head; 
+}
+
+
+
