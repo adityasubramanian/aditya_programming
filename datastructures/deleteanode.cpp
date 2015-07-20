c8f3671787dbedd7525cb9b4f5c837c5e0d84c31
Aditya <aditya.fly@gmail.com>
2015-07-19 23:12:16 -0700

    Updated file.

--- datastructures/deleteanode.cpp
+++ datastructures/deleteanode.cpp
@@ -5,23 +5,19 @@ struct Node {
 
 Node* Delete(Node* head, int position) {
   Node* temp_head = head; 
+  int counter = 0; 
   if (position == 0) {
     head = head -> next; 
     delete temp_head; 
     temp_head = NULL;
   } else {
-    int counter = 0; 
-    if (temp_head != NULL &&  ((counter+1) != position)) {
+    if (temp_head != NULL && (temp_head -> next !=NULL) && ((counter+1) != position)) {
       temp_head = temp_head -> next; 
       counter++;
-      if ((counter+1 == position)){
+      if ((counter+1) == position){
         temp_head -> next = (temp_head -> next) -> next; 
      } 
     } 
   }
   return head; 
 }
-
-
-
-
