609ea6b4d717cc1686f9ed65fd1cc71b9685d02e
Aditya Subramanian <subrmnn4@illinois.edu>
2015-10-29 14:07:36 -0500

    New problem

--- datastructures/postorder_traversal.java
+++ datastructures/postorder_traversal.java
@@ -1,1 +1,24 @@
-public class 
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+public Node leftNode() {
+	return left; 
+}
+public Node rightNode() {
+	return right; 
+}
+void Postorder(Node root) {
+	if (root != null) {
+		Postorder(root.leftNode());
+		Postorder(root.rightNode()); 
+		System.out.print(root.data, " "); 
+	}
+	return; 
+}
