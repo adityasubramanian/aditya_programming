d38ce542436b5ad9978e67f8025b765243d50d1c
Aditya <aditya.fly@gmail.com>
2015-10-30 00:14:00 -0500

    new problem

---
+++ datastructures/bst_insert.java
@@ -0,0 +1,24 @@
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node Insert(Node root,int value)
+    {
+        if (root == null) {
+            Node temp = new Node(); 
+            temp.data = value; 
+            root = temp; 
+        }
+        if (root.data > value) {        // Left side of the tree. 
+              root.left = Node.Insert(root.left,value);
+        }
+        if (root.data < value) {        // Right side of the tree. 
+            root.right = Node.Insert(root.right,value); 
+        }
+       return root; 
+    }
+
