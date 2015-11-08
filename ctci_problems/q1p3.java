30692e7a06b16dd22923c1eeedbf4d98104ff43a
Aditya <aditya.fly@gmail.com>
2015-11-07 18:38:57 -0600

    fix

--- ctci_problems/q1p3.java
+++ ctci_problems/q1p3.java
@@ -24,38 +24,20 @@ public class Question {
 		}
 		for (int i = 0; i < t.length(); ++i) {
 			int c = (int) t.charAt(i); 
-		}
-
-	}
-	public static boolean anagram(String s, String t) {
-		if (s.length() != t.length())
-			return false;
-		int[] letters = new int[128];
-		int num_unique_chars = 0;
-		int num_completed_t = 0;
-		char[] s_array = s.toCharArray();
-		for (char c : s_array) { // count number of each char in s.
-			if (letters[c] == 0)
-				++num_unique_chars;
-			++letters[c];
-		}
-		for (int i = 0; i < t.length(); ++i) {
-			int c = (int) t.charAt(i);
-			if (letters[c] == 0) { // Found more of char c in t than in s.
-				return false;
-			}
-			--letters[c];
 			if (letters[c] == 0) {
-				++num_completed_t;
+				return false; 
+			}
+			--letters[c]; 
+			if (letters[c] ==0) {
+				++num_completed_t; 
 				if (num_completed_t == num_unique_chars) {
-					return true;
-					//return i == t.length() - 1;
+					return true; 
 				}
 			}
 		}
-		return false;
+		return false; 
+
 	}	
-	
 	public static void main(String[] args) {
 		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
 		for (String[] pair : pairs) {
