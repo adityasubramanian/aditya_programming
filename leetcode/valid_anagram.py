490bb84071578262936740d72a4caf20bedd97c2
Aditya <aditya.fly@gmail.com>
2015-11-12 16:38:13 -0600

    new problem

---
+++ leetcode/valid_anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if (len(s) == len(t)):
+            word_one = sorted(s)
+            word_two = sorted(t)
+            return word_one == word_two 
+        else:
+            return False 
