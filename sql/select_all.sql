e9cb59634d3ae645f6bc9e83b04be3ac435c34c8
Aditya Subramanian <subrmnn4@illinois.edu>
2015-11-20 11:26:34 -0600

    fix

--- sql/select_all.sql
+++ sql/select_all.sql
@@ -2,7 +2,7 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 
-Given a City table, whose fields are described as
+Given a City table, whose fields are described as:
 
 +-------------+----------+
 | Field       | Type     |
