8499dbd31870d075577018589c8248e4629f4eb6
Aditya Subramanian <subrmnn4@illinois.edu>
2015-11-28 05:28:34 -0600

    updated template

---
+++ leetcode/min_stack.py
@@ -0,0 +1,44 @@
+'''
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+'''
+
+
+
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.stack.append(x)
+        if (len(self.stack2 is 0) or (x < self.stack2[-1])):
+            self.stack2.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        top = self.stack1[-1]
+
+    def top(self):
+        """
+        :rtype: int
+        """
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
