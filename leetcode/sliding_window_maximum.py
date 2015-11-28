39a7bebafba79199ce60d7495d9efe97daae0904
Aditya Subramanian <subrmnn4@illinois.edu>
2015-11-28 06:07:01 -0600

    new

--- leetcode/sliding_window_maximum.py
+++ leetcode/sliding_window_maximum.py
@@ -1,5 +1,6 @@
 '''
-Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the
+very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 
 For example,
 Given nums = [1,3,-1,-3,5,3,6,7], and k = 3.
@@ -15,7 +16,6 @@ Window position                Max
 Therefore, return the max sliding window as [3,3,5,5,6,7].
 '''
 
-
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
         """
@@ -23,4 +23,10 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        dequeue = collections.deque()
+        ans = []
+        for i in range(len(nums)):
+            while dequeue and nums[dequeue[-1]] <= nums[i]:
+                dequeue.pop()
+            dequeue.append(i)
         
