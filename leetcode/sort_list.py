4cdf933c284e54d28102e121ac2ce6cf4c72b759
Aditya Subramanian <subrmnn4@illinois.edu>
2015-12-05 16:40:43 -0600

    new change

--- leetcode/sort_list.py
+++ leetcode/sort_list.py
@@ -14,12 +14,11 @@ class Solution(object):
         cur = head
         while cur != None: 
             length += 1
-            curr = curr.next
-        return self.mergeSort(head,length) 
+            cur = cur.next
+        return self.mergeSort(head,length)
     def MergeSort(self,head,length):
-        if length == 1 or length ==0: 
+        if length == 1 or length ==0:
             return head
         prev = curr = head 
         for i in xrange(length/2): 
             curr = curr.next
-        
