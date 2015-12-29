a438ea9340c82200a852556b92b7f39c6ee9f5d0
Aditya Subramanian <subrmnn4@illinois.edu>
2015-12-28 22:33:05 -0800

    update

--- leetcode/bulb_switcher.py
+++ leetcode/bulb_switcher.py
@@ -1,8 +1,6 @@
 '''
 There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb. On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the nth round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
 '''
-
-
 import math
 class Solution(object):
     def bulbSwitch(self, n):
@@ -11,4 +9,4 @@ class Solution(object):
         :rtype: int
         """
         return int(math.sqrt(n))
-        
+        # only affects even, as two odd rounds would leave leftover to even
