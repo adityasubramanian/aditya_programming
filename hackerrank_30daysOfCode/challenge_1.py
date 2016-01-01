e4052a6fbc4dfcbae41e858aa59f71a35a90358f
Aditya Subramanian <subrmnn4@illinois.edu>
2016-01-01 14:29:30 -0800

    next problem

---
+++ hackerrank_30daysOfCode/challenge_1.py
@@ -0,0 +1,24 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+For this challenge, consider the following inputs (you don't need to read any input):
+
+5.35
+'a'
+false
+100
+"I am a code monkey"
+true
+17.3
+'c'
+"derp"
+'''
+
+print"""Primitive : double
+Primitive : char
+Primitive : boolean
+Primitive : int
+Referenced : String
+Primitive : boolean
+Primitive : double
+Primitive : char
+Referenced : String"""
