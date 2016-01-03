e34cad0dbf293048eab293211837abef856e4d1a
Aditya Subramanian <subrmnn4@illinois.edu>
2016-01-02 17:38:01 -0800

    day 2

---
+++ hackerrank_30daysOfCode/challenge_2.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function 
+meal = float(input())
+tip = int(input())
+tax = int(input())
+final = meal +((tip * meal)/100) + ((tax * meal)/100)
+print ('The final price of the meal is ${}.'.format(int(round(final,0))))
