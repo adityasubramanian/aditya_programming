6169cac5b514c004745d2f6276e43251d370918a
Aditya Subramanian <subrmnn4@illinois.edu>
2016-01-11 13:31:40 -0800

    new

---
+++ hackerrank_30daysOfCode/challenge_7.java
@@ -0,0 +1,31 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+   public static void main(String []argh)
+   {
+      Scanner in = new Scanner(System.in);
+      int N=in.nextInt();
+      in.nextLine();
+      HashMap<String,Integer> dict=new HashMap<String,Integer>();
+      for(int i=0;i<N;i++)
+      {
+         String name=in.nextLine();
+         int phone=in.nextInt();
+         in.nextLine();
+         dict.put(name,phone);    
+      }
+      while(in.hasNext())
+      {
+         String s=in.nextLine();
+         if (dict.get(s) == null) {
+             System.out.println("Not found");
+         } else {
+             System.out.println(s+"="+dict.get(s)); 
+         }
+      }
+   }
+}
+
+
