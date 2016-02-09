1bd9de06aec31b1df998638683555e2c43f44415
Aditya Subramanian <subrmnn4@illinois.edu>
2016-02-09 16:25:06 -0600

    fix

--- python/interchange_numbers.py
+++ python/interchange_numbers.py
@@ -1,11 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-
 def interchange():
-    a = int(raw_input())
-    b = int(raw_input())
-    a,b = b,a 
-    print(a) 
-    print(b) 
-    
-if __name__ == "__main__":
-    interchange() 
+	a = int(raw_input())
+	b = int(raw_input())
+	a,b = b,a
+	return a
+	return b 
+if __name__ =="__main__":
+	interchange()
